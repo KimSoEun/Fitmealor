4c9eebc77909d39cf8aa7518a11a1cea40eb3a6d
KimSoEun <thdms7947@naver.com>
2025-10-14 16:00:26 +0900

    Not import model classes in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
-from db.core import Base
+#from db.core import Base
+from .database import Base
 
 # 반드시 실제 모델들을 import해서 Base.metadata에 등록되도록!
-from .user import User
-from .meal import Meal
-from .recommendation import Recommendation
+# from .user import User
+# from .meal import Meal
+# from .recommendation import Recommendation
 # 필요한 모델 전부 import
 
