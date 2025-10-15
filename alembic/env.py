8dfb62032e3a207986ecf775fa68d628dfc55fb4
KimSoEun <thdms7947@naver.com>
2025-10-15 13:33:41 +0900

    Try 1 to fix URL error

--- alembic/env.py
+++ alembic/env.py
@@ -75,10 +75,10 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, 
-	    target_metadata=target_metadata,
-	    compare_type=True,
-	    compare_server_default=True
+            connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+        compare_server_default=True
         )
 
         with context.begin_transaction():
